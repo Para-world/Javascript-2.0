05583a6cc83ed0dcf8d01e10104e1039c8747673
Deepak kumar <dkumar39223@gmail.com>
2025-05-15 22:00:44 +0530

    loops

---
+++ 06 -Loops-in-JavaScript/script.js
@@ -0,0 +1,25 @@
+//  Loops in JavaScript
+//1. for loop 
+// . used when the number of iterations is known beforehand
+
+for (let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+
+// 2. while loop
+// . used when the loop should run as long as a condition is true.
+    let i = 0;
+    while (i < 5){
+        console.log(i);
+        i++;
+    }
+
+// 3. do-while loop
+// . runs the block of code once,then check the conditon.
+
+ let j = 0;
+    do {
+        console.log(j);
+        j++;
+    } while (j < 5);
